1ab893e152d4130bf5d033cb22cf22bb1d5168cf
Amith Sahani <95488369+amith1315@users.noreply.github.com>
2025-04-22 21:37:59 +0530

    Build Files

---
+++ build/CMakeFiles/MovieRatingSystem.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seed/Desktop/ooad/MovieRatingSystem/src/controller/Controller.cpp" "/home/seed/Desktop/ooad/MovieRatingSystem/build/CMakeFiles/MovieRatingSystem.dir/src/controller/Controller.cpp.o"
+  "/home/seed/Desktop/ooad/MovieRatingSystem/src/main.cpp" "/home/seed/Desktop/ooad/MovieRatingSystem/build/CMakeFiles/MovieRatingSystem.dir/src/main.cpp.o"
+  "/home/seed/Desktop/ooad/MovieRatingSystem/src/model/Model.cpp" "/home/seed/Desktop/ooad/MovieRatingSystem/build/CMakeFiles/MovieRatingSystem.dir/src/model/Model.cpp.o"
+  "/home/seed/Desktop/ooad/MovieRatingSystem/src/view/View.cpp" "/home/seed/Desktop/ooad/MovieRatingSystem/build/CMakeFiles/MovieRatingSystem.dir/src/view/View.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/controller"
+  "../src/model"
+  "../src/view"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MovieRatingSystem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MovieRatingSystem.dir/src/controller/Controller.cpp.o"
+  "CMakeFiles/MovieRatingSystem.dir/src/main.cpp.o"
+  "CMakeFiles/MovieRatingSystem.dir/src/model/Model.cpp.o"
+  "CMakeFiles/MovieRatingSystem.dir/src/view/View.cpp.o"
+  "MovieRatingSystem"
+  "MovieRatingSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MovieRatingSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
